f4c1144f8afc22c6d6dce8626a09d49934827c95
vovavish <vovavishniakov856@gmail.com>
2025-06-01 18:51:55 +0300

    fix search

--- rent_book_backend/src/book_search/book_search.controller.ts
+++ rent_book_backend/src/book_search/book_search.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { BookSearchService } from './book_search.service';
 import { BookSearchDto } from './dto/book_search.dto';
+import { Public } from "src/common/decorators";
 
 @Controller('book-search')
 export class BookSearchController {
   constructor(private readonly bookSearchService: BookSearchService) {}
 
   @Get()
+  @Public()
   async searchBooks(@Query() dto: BookSearchDto) {
     return this.bookSearchService.searchBooks(dto);
   }
